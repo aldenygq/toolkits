24e01f0d866368b6732ca772f567a2d4f8cbca88
guoqiang0114 <ygq13572549874@163.com>
2024-04-21 17:40:45 +0800

    add time

---
+++ time.go
@@ -0,0 +1,72 @@
+package toolkits
+
+import (
+	//"fmt"
+	"time"
+	
+	//analyticshub "google.golang.org/api/analyticshub/v1beta1"
+)
+const (
+	TIME_LAYOUT = "2006-01-02 15:04:05"
+)
+func GetDay() string {
+    now := time.Now().Format("2006-01-02")
+  
+   // fmt.Println("Current date:", now.Format("2006-01-02"))
+	return now
+}
+func StrToUnix(t string) int64 {
+	times, _ := time.Parse(t,TIME_LAYOUT)
+	return times.Unix()
+}
+
+
+//比较day1和day2日期顺序，如day1在前。返回0，如day2在前，返回1，如等于，返回2
+func CompareTwoDay(day1,day2 string) int64{
+	// 创建两个日期
+	d1, _ := time.Parse("2006-01-02", day1)
+	d2, _ := time.Parse("2006-01-02", day2)
+	
+	// 比较日期
+	if d1.Before(d2) {
+		return 0
+	} else if d1.After(d2) {
+		return 1
+	} else {
+		return 2
+	}
+	return 3
+}
+
+/*
+func main() {
+	day1 := "2024-01-04"
+	day2 := "2024-01-03"
+	fmt.Printf("num:%v\n",CompareTwoDay(day1,day2))
+	//fmt.Printf("day unix:%v\n",StrToUnix(getDay()))
+		currtime := time.Now()
+		fmt.Printf("now:%v\n",currtime)
+	
+	
+		t1:=time.Now().Year()        //年
+		t2:=time.Now().Month()       //月
+		t3:=time.Now().Day()         //日
+		t4:=time.Now().Hour()        //小时
+		t5:=time.Now().Minute()      //分钟
+		t6:=time.Now().Second()      //秒
+		t7:=time.Now().Nanosecond()  //纳秒
+	
+		fmt.Printf("year:%v\n",t1)
+		fmt.Printf("eng month:%v\n",t2)
+		fmt.Printf("int month:%v\n",int(t2))
+		fmt.Printf("day:%v\n",t3)
+		fmt.Printf("hour:%v\n",t4)
+		fmt.Printf("minute:%v\n",t5)
+		fmt.Printf("second:%v\n",t6)
+		fmt.Printf("nano second:%v\n",t7)
+	
+		//如果获取UTC时间，则可以使用time.UTC
+		currentTimeData:=time.Date(t1,t2,t3,t4,t5,t6,t7,time.Local) //获取当前时间，返回当前时间Time
+		fmt.Println(currentTimeData)    //打印结果：2017-04-11 12:52:52.794411287 +0800 CST
+}
+*/
--- verity.go
+++ verity.go
@@ -1,11 +1,43 @@
 package toolkits
 
 import (
+	"fmt"
 	"regexp"
 )
 
-func CheckMobile( mobile string) bool {
-	reg := `^1\d{10}$`
-	rgx := regexp.MustCompile(reg)
-	return rgx.MatchString(mobile)
+func CheckMobile(phone string) bool {
+	// ^1第一位为1
+	// [345789]{1} 后接一位345789 的数字
+	// \\d \d的转义 表示数字 {9} 接9位
+	// $ 结束符
+	if len(phone) != 11 {
+		return false
+	}
+	regRuler := "^1[3456789]{1}\\d{9}$"
+	
+	// 正则调用规则
+	reg := regexp.MustCompile(regRuler)
+	
+	// 返回 MatchString 是否匹配
+	return reg.MatchString(phone)
+}
+
+//邮箱是否合法
+func CheckEmail(email string) bool {
+	// 正则表达式匹配电子邮箱
+	// 匹配标准：至少一个字母数字或点的字符串，后面跟@，然后是至少一个字母数字或-的字符串，后面跟点，然后是至少两个字母的顶级域名。
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9.!#$%&'*+/=?^_` + "`" + `{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`)
+	return emailRegex.MatchString(email)
+}
+
+// 验证密码长度及复杂度，长度大于8
+func ValidatePassword(password string) bool {
+	// 至少8个字符，至少包含一个数字和一个特殊字符
+	pattern := `^(?=.*[0-9])(?=.*[!@#$%^&*])(.{8,})$`
+	matched, err := regexp.MatchString(pattern, password)
+	if err != nil {
+		fmt.Printf("密码不合法: %s\n", err)
+		return false
+	}
+	return matched
 }
