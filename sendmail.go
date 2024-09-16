b3203550b7e0b5530aa7e3083b096e862dd19244
Alden <ygq13572549874@163.com>
2024-09-16 22:22:23 +0800

    Update sendmail.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- sendmail.go
+++ sendmail.go
@@ -3,44 +3,102 @@ import (
         "crypto/tls"
         "net/smtp"
         "strings"
+        "fmt"
+        "errors"
+        "bytes"
+        "time"
+        "io/ioutil"
+        "encoding/base64"
 )
-func NewMailClient(from,passwd,stmpHost,stmpPort string) (*stmp.Client,error) {
-    conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%s", smtpHost, smtpPort), &tls.Config{})
-    if err != nil {
-        return nil,err
-    }
-    c, err := smtp.NewClient(conn, smtpHost)
-    if err != nil {
-        return nil,err
-    }
-    err = StmpAuth(c,from,passwd,smtpHost)
-    if err != nil {
-        return nil,err
-    }
-    return c,nil
+type Attachment struct {
+	name        string
+	contentType string
+	withFile    bool
 }
-func StmpAuth(c *smtp.Client,from,passwd,smtpHost string) error {
-    auth := smtp.PlainAuth("", from, password, smtpHost)
-    // 认证
+
+type Message struct {
+	from        string
+	to          []string
+	cc          []string
+	bcc         []string
+	subject     string
+	body        string
+	contentType string
+	attachment  Attachment
+}
+type Mail struct {
+	user     string
+	passwd string
+	host     string
+	port     string
+}
+
+func NewMailClient(mail Mail) (*smtp.Client,error) {
+    auth := smtp.PlainAuth("", mail.user, mail.passwd, mail.host)
+    conn, err := tls.Dial("tcp", fmt.Sprintf("%s:%s", mail.host, mail.port), &tls.Config{})
+    if err != nil {
+        fmt.Printf("connect mail failed:%v\n",err)
+        return nil,err
+    }
+    c, err := smtp.NewClient(conn, mail.host)
+    if err != nil {
+        fmt.Printf("new mail client  failed:%v\n",err)
+        return nil,err
+    }
     if auth != nil {
         if ok, _ := c.Extension("AUTH"); ok {
             if err = c.Auth(auth); err != nil {
-                return err
+                fmt.Printf("mail auth failed:%v\n",err)
+                return nil,err
             }
         } else {
-            return errors.New("auth not supported")
+            fmt.Printf("auth not supported")
+            return nil,errors.New("auth not supported")
         }
     }
-    return nil
+    return c,nil
 }
 
-func SendMail(c *smtp.Client,form,message string,to []string]) error {
+func SendMail(c *smtp.Client,content Message) error {
+	buffer := bytes.NewBuffer(nil)
+	boundary := "GoBoundary"
+	Header := make(map[string]string)
+	Header["From"] = content.from
+	Header["To"] = strings.Join(content.to, ";")
+	Header["Cc"] = strings.Join(content.cc, ";")
+	Header["Bcc"] = strings.Join(content.bcc, ";")
+	Header["Subject"] = content.subject
+	Header["Content-Type"] = "multipart/mixed;boundary=" + boundary
+	Header["Mime-Version"] = "1.0"
+	Header["Date"] = time.Now().String()
+	writeHeader(buffer, Header)
+
+	body := "\r\n--" + boundary + "\r\n"
+	body += "Content-Type:" + content.contentType + "\r\n"
+	body += "\r\n" + content.body + "\r\n"
+	buffer.WriteString(body)
+    if content.attachment.withFile {
+		attachment := "\r\n--" + boundary + "\r\n"
+		attachment += "Content-Transfer-Encoding:base64\r\n"
+		attachment += "Content-Disposition:attachment\r\n"
+		attachment += "Content-Type:" + content.attachment.contentType + ";name=\"" + content.attachment.name + "\"\r\n"
+		buffer.WriteString(attachment)
+		defer func() {
+			if err := recover(); err != nil {
+				panic(err)
+			}
+		}()
+		writeFile(buffer, content.attachment.name)
+	}
+
+	buffer.WriteString("\r\n--" + boundary + "--")
+
        // 发送邮件
-    if err = c.Mail(from); err != nil {
+    if err := c.Mail(content.from); err != nil {
         return err
     }
-    for _, addr := range to {
-        if err = c.Rcpt(addr); err != nil {
+    for _, addr := range content.to {
+        if err := c.Rcpt(addr); err != nil {
             return err
         }
     }
@@ -49,7 +107,7 @@ func SendMail(c *smtp.Client,form,message string,to []string]) error {
         return err
     }
 
-    _, err = w.Write([]byte(message))
+    _, err = w.Write([]byte(buffer.Bytes()))
     if err != nil {
         return err
     }
@@ -63,4 +121,29 @@ func SendMail(c *smtp.Client,form,message string,to []string]) error {
     }
 
     return nil
+}
+
+func writeFile(buffer *bytes.Buffer, fileName string) {
+	file, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err.Error())
+	}
+	payload := make([]byte, base64.StdEncoding.EncodedLen(len(file)))
+	base64.StdEncoding.Encode(payload, file)
+	buffer.WriteString("\r\n")
+	for index, line := 0, len(payload); index < line; index++ {
+		buffer.WriteByte(payload[index])
+		if (index+1)%76 == 0 {
+			buffer.WriteString("\r\n")
+		}
+	}
+}
+func writeHeader(buffer *bytes.Buffer, Header map[string]string) string {
+	header := ""
+	for key, value := range Header {
+		header += key + ":" + value + "\r\n"
+	}
+	header += "\r\n"
+	buffer.WriteString(header)
+	return header
 }
