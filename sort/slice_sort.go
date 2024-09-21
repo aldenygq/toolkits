0eaebcb2d4cb5aa13c63457aaf5182ed9d699243
Alden <ygq13572549874@163.com>
2024-09-21 21:57:53 +0800

    Update slice_sort.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- sort/slice_sort.go
+++ sort/slice_sort.go
@@ -2,6 +2,8 @@ package sort
 import (
   "sort"
 )
+
+//针对slice进行正向排序，slice元素中可能存在字母与数字组成的字符串，如：strs := []string{"banana", "apple1","apple3","apple34","apply","attach","cherry"}
 func SortSliceByChar(slc []string) []string {
 	// 使用sort.Slice进行自定义排序
 	sort.Slice(slc, func(i, j int) bool {
