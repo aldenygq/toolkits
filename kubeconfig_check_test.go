e25e58281bbc1c2176a2e0a1dfc4932406c47b3d
Alden <ygq13572549874@163.com>
2024-09-23 20:58:51 +0800

    Update kubeconfig_check_test.go
    Signed-off-by: Alden <ygq13572549874@163.com>

--- kubeconfig_check_test.go
+++ kubeconfig_check_test.go
@@ -5,7 +5,7 @@ import (
 )
 //go test -v -test.run Test_CheckKubeconfigByFile
 func Test_CheckKubeconfigByFile(t *testing.T) {
-    filepath := "/Users/mingyu/.kube/config-zjk-prod-odp"
+    filepath := "～/.kube/config
     strtime,unixtime,err := CheckKubeconfigByFile(filepath)
     if err != nil {
         fmt.Printf("check kubeconfig file failed:%v\n",err)
