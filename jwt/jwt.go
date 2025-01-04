16dc16d54db6e84c26abab88ca91897db4e21398
Alden <ygq13572549874@163.com>
2025-01-04 23:07:07 +0800

    Create jwt.go
    Signed-off-by: Alden <ygq13572549874@163.com>

---
+++ jwt/jwt.go
@@ -0,0 +1,84 @@
+package jwt
+
+import (
+	"errors"
+  "github.com/golang-jwt/jwt"
+  "time"
+)
+
+// JWT 签名结构
+type JWT struct {
+	SigningKey []byte
+}
+
+// 常量
+var (
+	TokenExpired     error = errors.New("token is expired")
+	TokenNotValidYet error = errors.New("token not active yet")
+	TokenMalformed   error = errors.New("that's not even a token")
+	TokenInvalid     error = errors.New("couldn't handle this token")
+)
+
+// CustomClaims 载荷，可以加一些自己需要的信息
+type CustomClaims struct {
+	ID       int64  `json:"id"`
+	Username string `json:"name"`
+	jwt.StandardClaims
+}
+
+// NewJWT 新建一个jwt实例
+func NewJWT(SignKey string) *JWT {
+	return &JWT{
+		[]byte(SignKey),
+	}
+}
+
+// CreateToken 生成一个token
+func (j *JWT) CreateToken(claims CustomClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.SigningKey)
+}
+
+// ParseToken 解析Token
+func (j *JWT) ParseToken(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		if ve, ok := err.(*jwt.ValidationError); ok {
+			if ve.Errors&jwt.ValidationErrorMalformed != 0 {
+				return nil, TokenMalformed
+			} else if ve.Errors&jwt.ValidationErrorExpired != 0 {
+				// Token is expired
+				return nil, TokenExpired
+			} else if ve.Errors&jwt.ValidationErrorNotValidYet != 0 {
+				return nil, TokenNotValidYet
+			} else {
+				return nil, TokenInvalid
+			}
+		}
+	}
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, TokenInvalid
+}
+
+// RefreshToken 更新Token
+func (j *JWT) RefreshToken(tokenString string) (string, error) {
+	jwt.TimeFunc = func() time.Time {
+		return time.Unix(0, 0)
+	}
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return j.SigningKey, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		jwt.TimeFunc = time.Now
+		claims.StandardClaims.ExpiresAt = time.Now().Add(1 * time.Hour).Unix()
+		return j.CreateToken(*claims)
+	}
+	return "", TokenInvalid
+}
