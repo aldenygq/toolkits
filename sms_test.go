ab7c8d38024be94e65991f98d24bbecebb0ac42e
Alden <ygq13572549874@163.com>
2025-02-05 20:43:49 +0800

    Create sms_test.go
    Signed-off-by: Alden <ygq13572549874@163.com>

---
+++ sms_test.go
@@ -0,0 +1,30 @@
+package toolkits
+
+import (
+	"fmt"
+	"log"
+    "testing"
+)
+
+func Test_SendSms(t *testing.T){
+    var cnf SmsServiceConfig
+    cnf.Type = "aliyun"
+    cnf.AccessKeyId = "xxxxxx"
+    cnf.AccessKeySecret = "xxxxxxxx"
+    cnf.Endpoint = "xxxxxxxx"
+    cnf.Region = "xxxxxxx"
+    cnf.SdkAppId = "xxxxxxx"
+    err := InitGlobalSmsClient(cnf)
+    if err != nil {
+        fmt.Printf("init sms client failed:%v\n",err)
+        return
+    }
+
+	// 发送短信
+	err = SendSms("+8613800138000", "your-sign-name", "your-template-code", "123456")
+	if err != nil {
+		log.Fatalf("failed to send SMS: %v", err)
+	    return
+    }
+	fmt.Println("SMS sent successfully")
+}
